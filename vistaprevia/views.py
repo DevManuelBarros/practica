253bcfdaa110a7110d8e8762ddfebe0622d0f5a6
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-11-11 00:11:20 -0300

    cambios 1

---
+++ vistaprevia/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
+
+#def index(request):
+#   return HttpResponse("Hola Mundo!")
+
+def index(request):
+    contenido = {'nombre_sitio' : 'LibrosOnline' }
+    para_minorista = {'tipo_usuario' : 'minorista', 'incremento' : '25'}
+    para_mayorista = {'tipo_usuario' : 'mayorista', 'incremento' : '10'}
+    return render(request, 'vistaprevia/index.html', {'contenido' : contenido,
+                                                    'para_minorista' : para_minorista,
+                                                    'para_mayorista' : para_mayorista})
