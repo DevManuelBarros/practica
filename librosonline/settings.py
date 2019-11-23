cfd8131b1edcf62aa8f7e2255435169a40e54b29
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-11-23 12:27:12 -0300

    modulo 4

--- librosonline/settings.py
+++ librosonline/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'librosonline',
+    'vistaprevia',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )   
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    Borrador = 'Borrador'
+    Publicado = 'Publicado'
+    Retirado = 'Retirado'
+    APROBACION_PRODUCTO = (
+        (Borrador, 'Borrador'),
+        (Publicado, 'Publicado'),
+        (Retirado, 'Retirado'),
+    )
+    producto = models.CharField(max_length=200)
+    fecha_publicacion = models.DateTimeField("Fecha de Publicación")
+    ruta_imagen = models.FileField(upload_to='fotos/%Y/%m/%d', default='defecto/defecto.png', blank=True, null=True)
+    estado = models.CharField(max_length=10, choices=APROBACION_PRODUCTO, default='Borrador')
+
+    def __str__(self):
+        return ('<%s => %s: %s, %s>' % (self.__class__.__name__, self.producto, self.fecha_publicacion, self.ruta_imagen))
+
