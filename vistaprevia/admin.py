2622c8749808b047696d662d9dc26f6a3042ff53
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-11-23 12:56:26 -0300

    Unidad 5

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Producto
-# Register your models here.
+from .models import Producto, Categoria
+
+
+def publicar(modeladmin, request, queryset):
+    queryset.update(estado='Publicado')
 
 class ProductoAdmin(admin.ModelAdmin):
-    fields = ['fecha_publicacion', 'producto', 'ruta_imagen']
-    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen']
+    fields = ['categoria', 'fecha_publicacion', 'producto', 'ruta_imagen', 'estado']
+    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen', 'tipo_de_producto']
     ordering = ['-fecha_publicacion']
     list_filter = ('producto', 'fecha_publicacion')
+    actions = [publicar]
 
+admin.site.register(Categoria)
 admin.site.register(Producto, ProductoAdmin)
--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.utils.html import format_html
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    def __str__(self):
+        return '%s' % self.nombre 
+
 
 class Producto(models.Model):
     Borrador = 'Borrador'
@@ -13,7 +21,16 @@ class Producto(models.Model):
     fecha_publicacion = models.DateTimeField("Fecha de Publicación")
     ruta_imagen = models.FileField(upload_to='fotos/%Y/%m/%d', default='defecto/defecto.png', blank=True, null=True)
     estado = models.CharField(max_length=10, choices=APROBACION_PRODUCTO, default='Borrador')
+    categoria = models.ManyToManyField(Categoria)
 
+    def tipo_de_producto(self):
+        if self.estado == 'Retirado':
+            return format_html('<span style="color: #f00;">{}</span>', self.estado,)
+        if self.estado == 'Borrador':
+            return format_html('<span style="color: #f0f;">{}</span>', self.estado,)
+        if self.estado == 'Publicado':
+            return format_html('<span style="color: #099;">{}</span>', self.estado,)
+    
     def __str__(self):
         return ('<%s => %s: %s, %s>' % (self.__class__.__name__, self.producto, self.fecha_publicacion, self.ruta_imagen))
 
