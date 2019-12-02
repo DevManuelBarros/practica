5b5dacaae5f90d68db78236756c49b7234b016c4
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-12-01 22:19:53 -0300

    parte5

--- vistaprevia/urls.py
+++ vistaprevia/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, contacto
+from .views import index, contacto, cargar_imagen
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
         path('', index, name='index'),
-        path('contacto/', contacto, name='contacto')
+        path('contacto/', contacto, name='contacto'),
+        path('cargar/', cargar_imagen, name='cargar'),
 ]
--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext
+from .models import Producto
+from .forms import CargaForm
+
 
 #def index(request):
 #   return HttpResponse("Hola Mundo!")
@@ -15,3 +18,19 @@ def index(request):
     return render(request, 'vistaprevia/index.html', {'contenido' : contenido,
                                                     'para_minorista' : para_minorista,
                                                     'para_mayorista' : para_mayorista})
+
+def cargar_imagen(request):
+    if request.method == 'POST':
+        form = CargaForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            producto = form.cleaned_data['producto']
+            fecha_publicacion = form.cleaned_data['fecha_publicacion']
+            ruta_imagen = form.cleaned_data['ruta_imagen']
+
+            newdoc = Producto(producto = producto, fecha_publicacion = fecha_publicacion, ruta_imagen=ruta_imagen)
+            newdoc.save()
+            return redirect('index')
+    else:
+        form = CargaForm()
+    return render(request, 'vistaprevia/formulario.html', {'form':form})
